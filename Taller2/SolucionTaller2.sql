574905035ed62a9df5bc0265ac61a83f32170173
samuelmr722 <34388939+samuelmr722@users.noreply.github.com>
2018-05-13 18:43:40 -0500

    Adición del punto 2
    Adición del punto 2

--- Taller2/SolucionTaller2.sql
+++ Taller2/SolucionTaller2.sql
@@ -1,7 +1,7 @@
 --Taller 2
 --Punto A
 ALTER TABLE "Aeronave" ADD Estado NVARCHAR2(20) NOT NULL;
-ALTER TABLE "Aeronave" ADD CONSTRAINT CK_ESTADO_AVION CHECK (Estado IN ('Vuelo','Tierra','Mantenimiento','Reparación'));
+ALTER TABLE "Aeronave" ADD CONSTRAINT CK_ESTADO_AVION CHECK (Estado IN ('Vuelo','Tierra','Mantenimiento','ReparaciÃ³n'));
 
 --Punto B
 ALTER TABLE "Aeronave" ADD Ultima_Ubicacion NVARCHAR2(255) NOT NULL;
@@ -43,8 +43,111 @@ From
             and ae."Estado"='Vuelo' --Validar que el avion este en transicion entre un aeropuerto y otro.
     ) envuelo 
       on origen."Ciudad"=envuelo."Ciudad" 
-Where  to_number(SUBSTR((origen."Hora_Estimada_Salida"-envuelo."Hora_Estimada_Llegada"),'11','3'))<='2'; --Validar que en menos de 2 horas, estará un avion en ese aeropuerto.
+Where  to_number(SUBSTR((origen."Hora_Estimada_Salida"-envuelo."Hora_Estimada_Llegada"),'11','3'))<='2'; --Validar que en menos de 2 horas, estarÃ¡ un avion en ese aeropuerto.
 
+--Punto 2
+create or replace PROCEDURE PROGRAMACION_TRIPULACION(i_Itinerario IN NUMBER)
+AS
+
+var_aeronave_disponible number := 0;
+var_can_auxi number := 0;
+var_can_sillas number := 0;
+var_ubi_act_tri nvarchar2(255) := '';
+var_mensaje nvarchar2(255) := 'Se asigno aeronave y tripulacion correctametne al vuelo';
+
+begin
+--Punto a
+--Asignar la aeronave a los vuelos confirmados, que se encuentran en el aeropuerto de salida, dos horas antes de la hora de salida
+
+/*UPDATE (SELECT i."Id_Aeronave" as sinavio,v."ID_AERONAVE" as aviodispo
+            FROM "LISTARAERONAVES" v 
+            INNER JOIN "Itinerario" i ON v."ID_VUELO" = i."Id_Vuelo"
+            where i."Id_Aeronave" is null and ROWNUM = 1) u
+SET u.sinavio = u.aviodispo;*/
+
+SELECT v."ID_AERONAVE" into var_aeronave_disponible
+FROM "LISTARAERONAVES" v 
+INNER JOIN "Itinerario" i ON v."ID_VUELO" = i."Id_Vuelo"
+where i."Id_Aeronave" is null and ROWNUM = 1 and i."Id" = i_Itinerario;
+
+if var_aeronave_disponible > 0 then
+    update "Itinerario" set "Id_Aeronave" = var_aeronave_disponible where "Id" = i_Itinerario;
+else
+    var_mensaje := 'No se asigno aeronave, por favor validar disponibilidad, y/o confirmacion del vuelo';
+GOTO salida;
+end if;
+--------------------------------------------------------------------------------------------------
+--Punto b
+--Recuperar el orgen del vuelo, para la busqueda de la tripulacion disponible
+select a."Ciudad"
+into var_ubi_act_tri
+from "Itinerario" i
+inner join "Vuelo" v on i."Id_Vuelo" = v."Id"
+inner join "Rutas" r on v."Id_Ruta" = r."Id"
+inner join "Aeropuerto" a on r."Id_Aeropuerto_Origen" = a."Id"
+where v."Id" = i_Itinerario;
+
+--Recuperar El piloto y el copiloto:
+insert into "PersonalAsignado" ("Rol","Id_Empleados","Id_Itinerario")
+select 'Piloto',e."Id",i_Itinerario
+from "Empleados" e inner join "Pilotos" p on e."Id" = p."Id_Empleados"
+where "Estado" = 'ACTIVO' and "Horas_Descanso_Ult_Vuelo" = 2 and "Ubicacion_Actual" = var_ubi_act_tri and ROWNUM = 1
+and p."Cargo" = 'Comandante';
+
+if sql%NOTFOUND then
+    var_mensaje := 'No se encontro piloto';
+GOTO salida;
+end if;
+
+insert into "PersonalAsignado" ("Rol","Id_Empleados","Id_Itinerario")
+select 'Copiloto',e."Id",i_Itinerario
+from "Empleados" e inner join "Pilotos" p on e."Id" = p."Id_Empleados"
+where "Estado" = 'ACTIVO' and "Horas_Descanso_Ult_Vuelo" = 2 and "Ubicacion_Actual" = var_ubi_act_tri and ROWNUM = 1
+and p."Cargo" = 'Primer Oficial';
+
+if sql%NOTFOUND then
+    var_mensaje := 'No se encontro copiloto';
+GOTO salida;
+end if;
+
+--Validacion de la cantidad de asientos para asignar tripulacion
+select ("Cant_Sillas_Negocios" + "Cant_Sillas_Economica") 
+into var_can_sillas 
+from "Aeronave" 
+where "Id" = var_aeronave_disponible;
+
+case 
+when var_can_sillas >= 19 and var_can_sillas < 50 then var_can_auxi := 1;
+when var_can_sillas >= 50 and var_can_sillas < 100 then var_can_auxi := 2;
+when var_can_sillas >= 100 and var_can_sillas < 180 then var_can_auxi := 3;
+when var_can_sillas >= 180 and var_can_sillas < 200 then var_can_auxi := 4;
+when var_can_sillas >= 200 and var_can_sillas < 250 then var_can_auxi := 5;
+when var_can_sillas >= 250 and var_can_sillas < 300 then var_can_auxi := 6;
+when var_can_sillas >= 300 and var_can_sillas < 350 then var_can_auxi := 7;
+when var_can_sillas >= 350 and var_can_sillas < 400 then var_can_auxi := 8;
+when var_can_sillas >= 450 and var_can_sillas < 500 then var_can_auxi := 9;
+when var_can_sillas >= 500 and var_can_sillas < 600 then var_can_auxi := 10;
+when var_can_sillas >= 650 and var_can_sillas < 700 then var_can_auxi := 11;
+when var_can_sillas >= 750 and var_can_sillas < 800 then var_can_auxi := 12;
+when var_can_sillas >= 850 and var_can_sillas < 853 then var_can_auxi := 18;
+else var_can_auxi := 1;
+end case;
+
+insert into "PersonalAsignado" ("Rol","Id_Empleados","Id_Itinerario")
+select 'Azafata',e."Id",i_Itinerario
+from "Empleados" e inner join "Pilotos" p on e."Id" = p."Id_Empleados"
+where "Estado" = 'ACTIVO' and "Horas_Descanso_Ult_Vuelo" = 2 and "Ubicacion_Actual" = var_ubi_act_tri and ROWNUM <= var_can_auxi
+and e."Sexo" = 'Mujer';
+
+if sql%NOTFOUND then
+    var_mensaje := 'No se encontro auxiliares';
+GOTO salida;
+end if;
+
+dbms_output.put_line(var_mensaje);
+<<salida>>
+dbms_output.put_line(var_mensaje);
+end;
 
 --Punto 4
 ALTER TABLE "PersonalAsignado" ADD CONSTRAINT CK_ROL CHECK ("Rol" IN ('Piloto','Copiloto','Azafata'));
